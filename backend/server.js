54e48f886eb75c40f4db453c1deed5fc10495ce6
vzhang03 <vzhang@vassar.edu>
2025-01-24 20:40:09 -0500

    Adjusted directory structure to support database

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+require('dotenv').config(); // Load environment variables from .env file
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+// Use the environment variable for the MongoDB URI
+const url = process.env.MONGO_URL;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(url, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server (optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+
+run().catch(console.dir);
